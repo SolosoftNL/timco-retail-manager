73cdeac206c3420a581896b6ae61188cc563c845
davidhenley <henleydr@gmail.com>
2019-10-31 01:30:11 -0500

    Remove unnecessary namespaces

--- TimCo.UI/Bootstrapper.cs
+++ TimCo.UI/Bootstrapper.cs
@@ -3,7 +3,6 @@ using Caliburn.Micro;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Windows;
 using TimCo.UI.Infrastructure;
 using TimCo.UI.Shell;
--- TimCo.UI/Shell/ShellViewModel.cs
+++ TimCo.UI/Shell/ShellViewModel.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace TimCo.UI.Shell
+﻿namespace TimCo.UI.Shell
 {
     class ShellViewModel : IShell
     {
